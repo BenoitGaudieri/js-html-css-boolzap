2c57052f1d7a18093f94fcf2f8faf6ef7097e2de
BenoitGaudieri <benalek@gmail.com>
2020-04-21 15:41:45 +0200

    feat. answer generator, last message in userdiv

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
     // Message references
-    let messageArr = ["Ciao come stai?", "Questo è il secondo messaggio"];
+    let convStart = ["Hello!", "Please ask me anything."];
     let newMessage = $(".js-chat--input");
     let text = newMessage.val();
-    let answer = "Ok!";
 
     // Sidebar references
     let sidebar = $(".js-sidebar__content");
@@ -45,9 +44,12 @@ $(document).ready(function () {
 
     // Reference to the active
     let messageDivActive = $(".js-conversation.active .js-content__messages");
+    let allMessageDiv = $(
+        ".content__messages--margin .js-conversation .js-content__messages"
+    );
 
-    // Populating messageDisplay with array of old messages
-    for (let i = 0; i < messageArr.length; i++) {
+    // Initial messages
+    for (let i = 0; i < convStart.length; i++) {
         // Previously:
         // let oldMessage = $(".js-template .message").clone();
         // var oldTimestamp = $(".js-template .message--timestamp").clone();
@@ -59,7 +61,7 @@ $(document).ready(function () {
         // messageDiv.append(oldMessage);
 
         // Refactored:
-        msgDisplay(messageArr[i]);
+        msgDisplay(convStart[i], false, true);
     }
 
     // Toggle send icon
@@ -79,6 +81,7 @@ $(document).ready(function () {
         }
     });
 
+    // Change active selected conversation and user
     $(".js-user--div").click(function () {
         // reference for the switch
         var clicked = $(this).attr("data-conversation");
@@ -92,12 +95,60 @@ $(document).ready(function () {
         $(".js-conversation[data-conversation='" + clicked + "']").addClass(
             "active"
         );
+        // Refresh active div reference
         messageDivActive = $(".js-conversation.active .js-content__messages");
+        // Change avatar in header
+        $(".content__header--left .user-pic").attr(
+            "src",
+            "img/avatar_" + (parseInt(clicked) + 1) + ".jpg"
+        );
+        // Change username
+        $(".content__header--left .user--info .user--username").text(
+            userList[clicked]
+        );
     });
 
     // Remove message
     $("body").on("click", ".fa-trash-alt", function () {
         $(this).parent().remove();
+    });
+
+    /**
+     * Searchbar
+     */
+
+    //  Searchbar logic
+    searchBar.keyup(function () {
+        let searchInput = $(this).val().toLowerCase().trim();
+        // Previously:
+        // if (searchInput != "") {
+        //     sidebar.children().hide();
+        //     for (let i = 0; i < userList.length; i++) {
+        //         if (userList[i].includes(searchInput)) {
+        //             // nth-child solution
+        //             let selectedUser = $(
+        //                 ".js-user--div:nth-child(" + (i + 1) + ")"
+        //             );
+        //             // eq solution
+        //             // sidebar.children().eq(i).show();
+        //             selectedUser.show();
+        //         }
+        //     }
+        // } else {
+        //     sidebar.children().show();
+        // }
+        // Refactored:
+        $(".js-sidebar__content .js-user--div").each(function () {
+            // Nome contatto attuale nel loop
+            var name = $(this).find(".user--username").text().toLowerCase();
+
+            // verifica input con nome contatto
+            if (name.includes(searchInput)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
     });
 
     /**
@@ -107,7 +158,6 @@ $(document).ready(function () {
     /**
      * invoke msgDisplay passing the message
      */
-    // TODO: select only active conv
     function sendMessage() {
         text = newMessage.val();
         if (text) {
@@ -120,12 +170,10 @@ $(document).ready(function () {
      * Display message
      * @text {bool} If false it's an automated reply sent by the function itself
      * @sent {bool} If false it's not my-message
+     * @init {bool} If true it's the first message
      */
-
-    //  TODO: add active = false - if active then append to messageDivActive
-    function msgDisplay(text = false, sent = false) {
+    function msgDisplay(text = false, sent = false, init = false) {
         var elementNew = $(".js-template .message").clone();
-        console.table(elementNew);
 
         // There is a message input
         if (text) {
@@ -142,15 +190,236 @@ $(document).ready(function () {
                     // Bot reply
                     setTimeout(msgDisplay, 1000);
                 }
-                messageDivActive.append(elementNew);
+
+                if (init) {
+                    allMessageDiv.append(elementNew);
+                } else {
+                    messageDivActive.append(elementNew);
+                }
 
                 // empty input val
                 newMessage.val("");
             }
         } else {
+            // Bot side
+            // Answer generator
+            var answer = "";
+            var data = $(".active").attr("data-conversation");
+            console.log(data);
+
+            answer += randomFromArr([
+                randomFromArr([
+                    "What a nice day!",
+                    "It's sunny today.",
+                    "It's a sunny day out.",
+                    "It's such a nice day.",
+                    "It's such a great day to be alive!",
+                    "This is a happy kind of day.",
+                    "I feel great.",
+                    "Ooh, I'm feeling fine.",
+                    "I'm feeling awesome.",
+                    "Hey, this is great!",
+                    "I'm so glad I came here.",
+                    "I regret nothing!",
+                    "Regrets are pointless.",
+                    "I have no regrets.",
+                    "This is what I wanted!",
+                    "Everything's going just fine.",
+                    "I can't wait!",
+                    "Things are going smoothly.",
+                    "I'm just happy to be here.",
+                    "Well, this is " +
+                        randomFromArr([
+                            "unexpected",
+                            "awkward",
+                            "fun",
+                            "just funny",
+                            "strange",
+                            "interesting",
+                            "odd",
+                            "peculiar",
+                            "weird",
+                        ]) +
+                        ".",
+                    "I know the meaning of life!",
+                    "I didn't expect this!",
+                    "I'm glad someone understands me.",
+                    "I'm glad someone likes me for who I am.",
+                    "I love my friends!",
+                    "Life is good!",
+                    "I could picture myself spending the rest of my life here.",
+                    "I should meet new friends!",
+                    "I feel loved.",
+                    "I feel almighty!",
+                    "I matter.",
+                    "This place is nice.",
+                    "Everybody's great in some way!",
+                    "I hope " +
+                        randomFromArr(["they", "she", "he"]) +
+                        "'ll like me!",
+                    "I wonder what happens next!",
+                    "It's all going to be alright.",
+                    "It'll all be alright in the end. I just know it.",
+                    "This is actually okay.",
+                    "I love the whole world!",
+                    "The world is a big place!",
+                    "The world is amazing!",
+                    "So it has come to this.",
+                    "Well this is an interesting development.",
+                    "Let's see what happens next.",
+                    "Oh hey. I found me.",
+                    "I NEED to blog about this.",
+                ]),
+                randomFromArr([
+                    "I should take a self-help course!",
+                    "I'm doing alright.",
+                    "My job is pretty fulfilling.",
+                    "I don't really worry.",
+                    "Worrying is pointless!",
+                    "This is cooler than I expected!",
+                    "Haha, this is just like me.",
+                    "Wouldn't it be awesome if I was " +
+                        randomFromArr([
+                            "a pirate",
+                            "a dolphin",
+                            "an unicorn",
+                            "a panda",
+                            "a cyborg",
+                            "a robot",
+                            "a superhero",
+                            "invisible",
+                        ]) +
+                        "?",
+                    "I'm pretty self-confident.",
+                    "I'm a pretty big deal.",
+                    "I'm pretty extreme.",
+                    randomFromArr([
+                        "You want a piece of this?",
+                        "I'm all business, all the time.",
+                        "I vibrate through walls.",
+                        "This is going to be gay as hell.",
+                        "We're making this happen!",
+                    ]),
+                    "I mean, wow.",
+                    "Yep. Just a regular human person. Nothing to see here.",
+                    "I want to become even better!",
+                    "I should learn a new " +
+                        randomFromArr(["skill", "language"]) +
+                        "!",
+                    "I hope I'll become a nice old person.",
+                    "I kinda miss my youth!",
+                    "I should call " +
+                        randomFromArr(["her", "him"]) +
+                        ' and say "I love you"!',
+                    "Maybe I should call my parents?",
+                    "You can always better yourself.",
+                    "Do I really suck at " +
+                        randomFromArr([
+                            "singing",
+                            "painting",
+                            "dancing",
+                            "writing",
+                            "video-games",
+                            "maths",
+                        ]) +
+                        "?",
+                    "I'm secretly " +
+                        randomFromArr([
+                            "super-hardcore",
+                            "perfectly normal",
+                            "perfectly ordinary",
+                            "peeking over your shoulder right now",
+                            "a wizard",
+                            "the smartest person in the world",
+                            "the wisest person in the world",
+                            "the most important person in the world",
+                            "a spider",
+                            "a robot",
+                            "a midget",
+                            "a very ancient ghost",
+                            "an extra-terrestrial",
+                            "a tree",
+                            "a flower",
+                            "a shark",
+                            "a bear",
+                            "my own cousin",
+                            "an astral monstrosity",
+                            "a secret",
+                        ]) +
+                        "!",
+                    "H-here I go!",
+                    "This place would look good on fire!",
+                    "I don't worry, because I know nothing matters in the end.",
+                    "Well, I'm glad nobody can read my mind.",
+                ]),
+                randomFromArr([
+                    "Hmm! I should get",
+                    "I know what would be awesome...",
+                    "Time for",
+                    "I need",
+                    "You know what? I need",
+                    "Know what I need? I'll tell you - ",
+                ]) +
+                    " " +
+                    randomFromArr([
+                        "some new shoes",
+                        "a new TV",
+                        "a new computer",
+                        "a new car",
+                        "a bigger house",
+                        "a cooler job",
+                        "a lover",
+                        "more pets",
+                        "a makeover",
+                        "a good movie",
+                        "a nice dinner in town",
+                    ]) +
+                    "!",
+                randomFromArr([
+                    "This is the best relationship ever.",
+                    "I love being married!",
+                    "Marriage isn't as bad as they make it out to be!",
+                    "This relationship is awesome!",
+                    "I love my family.",
+                    "I love doing stuff with my family.",
+                ]),
+                randomFromArr([
+                    randomFromArr([
+                        "I really, really like",
+                        "I can't get enough of",
+                        "I'm going to get more of",
+                        "I wonder what they put in",
+                    ]) +
+                        " " +
+                        randomFromArr([
+                            "this hamburger",
+                            "this steak",
+                            "this salad",
+                            "this pasta",
+                            "this sandwich",
+                            "this pizza",
+                            "this meal",
+                            "this beer",
+                            "this soda",
+                        ]) +
+                        ".",
+                    randomFromArr([
+                        "Haha, I love that show!",
+                        "That show is confusing!",
+                        "That show is hilarious.",
+                        "That's a silly show, but there's nothing on TV anyway.",
+                        "Oooh, my parents used to watch that show!",
+                        "There's nothing on TV!",
+                        "That movie's plot is hilariously bad.",
+                        "I've never seen that movie before!",
+                    ]),
+                ]),
+            ]);
             elementNew.children(".message--text").text(answer);
             elementNew.children(".message--timestamp").text(timestampCalc());
             messageDivActive.append(elementNew);
+            // Change last message in user div
+            $(".js-user--div.active").find(".user--last-message").text(answer);
         }
         scrollDown();
     }
@@ -202,39 +471,13 @@ $(document).ready(function () {
     }
 
     /**
-     * Searchbar
+     * Choose a random array
+     * @arr {arr} pass an array as max
+     * @min {int} min
      */
-
-    //  Searchbar logic
-    searchBar.keyup(function () {
-        let searchInput = $(this).val().toLowerCase().trim();
-        // if (searchInput != "") {
-        //     sidebar.children().hide();
-        //     for (let i = 0; i < userList.length; i++) {
-        //         if (userList[i].includes(searchInput)) {
-        //             // nth-child solution
-        //             let selectedUser = $(
-        //                 ".js-user--div:nth-child(" + (i + 1) + ")"
-        //             );
-        //             // eq solution
-        //             // sidebar.children().eq(i).show();
-        //             selectedUser.show();
-        //         }
-        //     }
-        // } else {
-        //     sidebar.children().show();
-        // }
-
-        $(".js-sidebar__content .js-user--div").each(function () {
-            // Nome contatto attuale nel loop
-            var name = $(this).find(".user--username").text().toLowerCase();
-
-            // verifica input con nome contatto
-            if (name.includes(searchInput)) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-    });
+    function randomFromArr(arr, min = 0) {
+        return arr[
+            Math.floor(Math.random() * Math.floor(arr.length - min)) + min
+        ];
+    }
 }); // end Doc ready
